ec4a15b3004909db80514005e8b973ed2c602538
Yaseen Alkhafaji <eng.yaseen89@outlook.com>
2020-04-23 14:30:58 -0700

    Update repository

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -4,25 +4,15 @@ import Icon from './icon'
 import {cn} from '../lib/helpers'
 
 import styles from './header.module.css'
+import Navbar from './navbar'
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
       <div className={styles.branding}>
         <Link to='/'>{siteTitle}</Link>
+        <Navbar />
       </div>
-
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to='/archive/'>Archive</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
   </div>
 )
